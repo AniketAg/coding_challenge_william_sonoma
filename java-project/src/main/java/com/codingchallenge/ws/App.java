fcc63760c9b146b0c8b1d03b1a8fe4060c7efe94
Aniket Agarwal <agarwaan@tcd.ie>
2019-12-12 11:28:52 -0800

    Created helper file

--- java-project/src/main/java/com/codingchallenge/ws/App.java
+++ java-project/src/main/java/com/codingchallenge/ws/App.java
@@ -1,13 +1,25 @@
 package com.codingchallenge.ws;
 
+import java.util.*;
+
 /**
- * Hello world!
- *
+ * @author: Aniket Agarwal
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Coding Challenge by Williams Sonoma\n Please enter the values: \n" );
+        
+        List zip_code_range =  new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(scanner.nextLine());
+        while(input.hasNext())
+            zip_code_range.add(input.next());
+        scanner.close();
+        
+        
+
+        
     }
 }
---
+++ java-project/src/main/java/com/codingchallenge/ws/helper.java
@@ -0,0 +1,11 @@
+package com.codingchallenge.ws;
+
+import java.util.*;
+
+public class helper {
+    private int start,end ;
+    public String output(){
+        return "[" + start + "," + end + "]";
+    }
+}
+
